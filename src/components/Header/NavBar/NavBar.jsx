870e8192ef8ede55ee0039f9045aaabca1f2be49
unknown <you@example.com>
2024-03-26 13:06:04 +0100

    Removed LogOut component and logout route, replacing them with an alert upon user logout.

--- src/components/Header/NavBar/NavBar.jsx
+++ src/components/Header/NavBar/NavBar.jsx
@@ -4,22 +4,35 @@ import { UserAuth } from "../../../context/AuthContext";
 
 const NavBar = () => {
 
-  const { user, rol } = UserAuth();
+  const { user, rol, setRol, logOut } = UserAuth();
+
+
+
+  const cerrarSesion = async () => {
+    try {
+      await logOut();
+      alert("Cerraste sesión correctamente. ¡Hasta pronto!")
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+
 
   return (
 
     <ul>
-     
+
       <li>
         <NavLink to="/dashboard">Dashboard</NavLink>
       </li>
-    {rol &&
-      <li>
-        <NavLink to="/users">Usuarios</NavLink>
-      </li>
-    }
-      <li>
-        <NavLink to="/logout">LogOut</NavLink>
+      {rol &&
+        <li>
+          <NavLink to="/users">Usuarios</NavLink>
+        </li>
+      }
+      <li onClick={cerrarSesion}>
+        <a href="">LogOut</a>
       </li>
     </ul>
   )
