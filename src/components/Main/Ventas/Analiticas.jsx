ef6a20dc5a9f00960574be0fad74509315ea7b83
santivediap <santivediap@gmail.com>
2024-04-02 18:07:38 +0200

    Create styles and structure of Analitica component

--- src/components/Main/Ventas/Analiticas.jsx
+++ src/components/Main/Ventas/Analiticas.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import VentasChart from './VentasChart'
+import Breadcrumb from '../Breadcrum/Breadcrum'
+import { Link } from 'react-router-dom'
 
 const Analiticas = () => {
   return (
-    <div>
-      <h1>
-        ==========VENTAS/ANALITICAS===============
-        </h1>
-    </div>
+    <section className='analiticas-main-container'>
+      <article className='breadcrum'>
+        <Breadcrumb />
+      </article>
+
+      <article className='analiticas-container'>
+        <h1>Analítica de ventas</h1>
+
+        <div className='machines-type'>
+          <Link to={ "/ventas/analiticas/cafe" }>Máquina de café</Link>
+          <span>|</span>
+          <Link to={ "/ventas/analiticas/snacks" }>Máquina de snacks</Link>
+          <span>|</span>
+          <Link to={ "/ventas/analiticas/vitrina" }>Vitrina inteligente</Link>
+        </div>
+
+        <div className='ventas-graphic'>
+          <VentasChart />
+        </div>
+      </article>
+    </section>
   )
 }
 
